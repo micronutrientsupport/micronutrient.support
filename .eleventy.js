cc88978ec9791872000cc4bf344c27c09527bde4
Bean, Andrew K <andan@bsg.ac.uk>
2022-01-18 12:42:27 +0000

    feat: add basic test config for netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(true);
 
+    eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("css");
 };
